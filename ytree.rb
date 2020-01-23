4f689b1e535b05563bec571dc6884682023ef6ae
blogabe <6888675+blogabe@users.noreply.github.com>
2020-01-22 20:30:45 -0800

    fix sha

--- ytree.rb
+++ ytree.rb
@@ -2,7 +2,7 @@ class Ytree < Formula
   desc "Ytree; a UNIX Filemanager - a (curses-based) file manager similar to DOS Xtree(tm)"
   homepage "http://www.han.de/~werner/ytree.html"
   url "https://www.han.de/~werner/ytree-2.00.tar.gz"
-  sha256 "763e8c2af528174cd738ed8f2eca62579e3edef57ccbd242be7cf181ae15d251"
+  sha256 "da8ecaf8e15f8b0d3077ea88da8db9cd607f72ed2a51e91a24d6ef3ff017c6bf"
 
   def install
     mkpath "#{buildpath}/hb-pkg/bin"
