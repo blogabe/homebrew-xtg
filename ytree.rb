6a8f444357be199228197a8b1ae0a21c9eebab1f
blogabe <6888675+blogabe@users.noreply.github.com>
2026-03-19 22:16:19 -0700

    Update ytree.rb
    version and modern homebrew notation

--- ytree.rb
+++ ytree.rb
@@ -1,26 +1,22 @@
 class Ytree < Formula
-  desc "Ytree; a UNIX Filemanager - a (curses-based) file manager similar to DOS Xtree(tm)"
-  homepage "http://www.han.de/~werner/ytree.html"
-  url "https://www.han.de/~werner/ytree-2.02.tar.gz"
-  sha256 "e5bf41ab6bee59cf1e8fc0ca429e9a18e6aa83823f30f5807042b04c88a0e0ec"
+  desc "Curses-based file manager similar to DOS XTree"
+  homepage "https://www.han.de/~werner/ytree.html"
+  url "https://www.han.de/~werner/ytree-2.10.tar.gz"
+  sha256 "3bbbbd32f568cdae3e03fc735b1783d8cba605a2ca6056d1b971143d7ddd517d"
+  license "GPL-2.0-or-later"
 
   def install
-    mkpath "#{buildpath}/hb-pkg/bin"
-    mkpath "#{buildpath}/hb-pkg/share/man/man1"
-    mkpath "#{buildpath}/hb-pkg/share/man/es/man1"
     inreplace "Makefile" do |s|
-      s.gsub! "DESTDIR     = /usr", "DESTDIR     = #{buildpath}/hb-pkg"
+      s.gsub! "DESTDIR     = /usr", "DESTDIR     = #{prefix}"
       s.gsub! "COLOR       = -DCOLOR_SUPPORT", "#COLOR       = -DCOLOR_SUPPORT"
       s.gsub! "READLINE    = -DREADLINE_SUPPORT", "#READLINE    = -DREADLINE_SUPPORT"
-      s.gsub! "CFLAGS      += -D_GNU_SOURCE $(COLOR) $(CLOCK) $(READLINE) $(ADD_CFLAGS)", "#CFLAGS      += -D_GNU_SOURCE $(COLOR) $(CLOCK) $(READLINE) $(ADD_CFLAGS)"
+      s.gsub! "CFLAGS      += -D_GNU_SOURCE $(COLOR) $(CLOCK) $(READLINE) $(ADD_CFLAGS)",
+              "#CFLAGS      += -D_GNU_SOURCE $(COLOR) $(CLOCK) $(READLINE) $(ADD_CFLAGS)"
       s.gsub! "LDFLAGS     += -lncurses -lreadline", "#LDFLAGS     += -lncurses -lreadline"
       s.gsub! "#CFLAGS      = -DCOLOR_SUPPORT -DNCURSES", "CFLAGS      = -DCOLOR_SUPPORT -DNCURSES"
       s.gsub! "#LDFLAGS     = -lncurses", "LDFLAGS     = -lncurses"
     end
     system "make", "install"
-
-    cd "#{buildpath}/hb-pkg"
-    prefix.install "bin", "share"
-    (share/"ytree").install "#{buildpath}/ytree.conf"
+    (share/"ytree").install "ytree.conf"
   end
 end
